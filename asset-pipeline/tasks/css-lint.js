da7962bd23dba81b65912fbef570b5cee5a92ada
tsm <attila.egyed.91@gmail.com>
2016-01-16 15:31:55 +0100

    postcss-stream-filter package changed to postcss-filter-stream

--- asset-pipeline/tasks/css-lint.js
+++ asset-pipeline/tasks/css-lint.js
@@ -9,7 +9,7 @@ import scss from 'postcss-scss';
 import stylelint from 'stylelint';
 import doiuse from 'doiuse';
 import colorguard from 'colorguard';
-import streamFilter from 'postcss-stream-filter';
+import filterStream from 'postcss-filter-stream';
 
 const paths = {
   src: path.join(config.SRC_DIR, config.cssLint.src, config.cssLint.glob)
@@ -20,7 +20,7 @@ const processors = [
     browsers: config.css.autoprefixerBrowsers,
     ignoreFiles: [config.cssLint.ignoreGlob]
   }),
-  streamFilter(config.cssLint.ignoreGlob, colorguard()),
+  filterStream(config.cssLint.ignoreGlob, colorguard()),
   stylelint(),
   reporter({ clearMessages: true })
 ];
