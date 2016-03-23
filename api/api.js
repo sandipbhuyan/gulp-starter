fbc28b1ad63f4888c706c53ed675194b460ee961
tsm <attila.egyed.91@gmail.com>
2016-03-23 22:34:51 +0100

    remove morgan from api

--- api/api.js
+++ api/api.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const api = express();
 
-api.use(logger('dev'));
 api.use(bodyParser.json());
 
 require('./sample/routes')(api);
