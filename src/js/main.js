719e03c4993f7a8a1448142472e853c0cf35e8f4
sandipkumarbhuyan <sandipbhuyan@gmail.com>
2017-10-06 23:23:05 +0530

    [TASK]: Initialize the project

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
-window.$ = window.jQuery = $;
+
+// window.$ = window.jQuery = $;
 
 $.get('http://localhost:3000/api/sample', (res) => {
   console.log('Response from sample api: ', res);
